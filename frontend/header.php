ea42604fff6df56c1c2dfba0a4e4da3735c5fbb2
THEJA SEKHAR REDDY GUNDLOORU <94587663+ThejaReddy1@users.noreply.github.com>
2023-02-10 21:46:57 +0530

    commit3

--- frontend/header.php
+++ frontend/header.php
@@ -35,14 +35,14 @@
     <header class="header-section">
         <div class="container">
             <div class="logo">
-                <a href="./index.html">
+                <a href="../index.html">
                     <img src="img/logo.png" alt="" height="70px" width="200px">
                 </a>
             </div>
             <div class="nav-menu">
                 <nav class="mainmenu mobile-menu">
                     <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li class="active"><a href="../index.html">Home</a></li>
                         <li><a href="./about-us.html">About</a></li>
                         <li><a href="./speaker.html">Conveners</a>
                             <!-- <ul class="dropdown">
